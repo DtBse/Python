4b754304e5a3b8bbb248b23bb1336eb312a7627a
DtBse <91613150+DtBse@users.noreply.github.com>
2022-06-19 19:45:45 +0300

    Add files via upload

---
+++ hwww.py
@@ -0,0 +1,13 @@
+result = 0
+def divider(a, b, self):
+ if(a < b):
+  raise ValueError
+ if(b > 100):
+  raise IndexError
+  return a/b
+data = {10, 2, 2, 5, 123, 4, 18, 0, 15, 8, 4}
+for key in data:
+ res = divider(key, data)
+ result.append(res)
+
+print(result)
